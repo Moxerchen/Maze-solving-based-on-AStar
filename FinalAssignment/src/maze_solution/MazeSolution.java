d3777ac7ad49b08ba94669ae6e826cd30101356f
Moxerchen <83894313+Moxerchen@users.noreply.github.com>
2021-07-10 15:13:53 +0800

    Add files via upload
    进行了一些修改，因为原来的可移动条件会导致某些点重复添加进openList，虽然这是过去计算最短路径的方法，但这会导致求解最短路径数量巨大的迷宫的运行时间大量增加。修改条件后难以计算最短路径的条数，目前的思路是通过已有的Gx进行计算，从终点回溯到起点，标记出所有可行的岔路，由此计算出最短路径的条数。

--- FinalAssignment/src/maze_solution/MazeSolution.java
+++ FinalAssignment/src/maze_solution/MazeSolution.java
@@ -1,31 +1,57 @@
 package maze_solution;
 
+import java.io.File;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class MazeSolution {
 
-	public static void main(String[] args){
-		int[][] map = {
-				{ 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 },
-				{ 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1 },
-				{ 1, 0, 1, 1, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 1 },
-				{ 1, 0, 1, 1, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 1 },
-				{ 1, 0, 0, 0, 0, 1, 1, 1, 1, 1, 0, 0, 1, 1, 1 },
-				{ 1, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 1, 1, 0, 1 },
-				{ 1, 0, 0, 1, 0, 0, 0, 0, 1, 0, 1, 1, 0, 0, 1 },
-				{ 1, 0, 0, 1, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 1 },
-				{ 1, 0, 0, 1, 0, 0, 0, 0, 1, 0, 1, 1, 1, 0, 1 },
-				{ 1, 0, 0, 1, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 1 },
-				{ 1, 0, 0, 1, 0, 0, 0, 0, 1, 1, 1, 0, 1, 0, 1 },
-				{ 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1 },
-				{ 1, 0, 0, 1, 0, 0, 0, 1, 0, 1, 1, 1, 1, 0, 1 },
-				{ 1, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0 },
-				{ 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 }};
+	public static void main(String[] args) throws Exception{
+		Scanner in = new Scanner(System.in);
+		System.out.println("Please enter the size and name of your maze");
+		int n = in.nextInt();
+		String filename = in.next();
+		File file=new File(filename);
 		
-		MapStructure QuestionMap = new MapStructure(map, map.length, new Point(0, 0), new Point(4, 5));
+		Scanner input = new Scanner(file);
+		int[][] map = new int[n][n];
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < n; j++) {
+				map[i][j] = input.nextInt();
+			}
+		}
+		
+		long startTime = System.currentTimeMillis();
+//		int[][] map = {
+//				{ 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 },
+//				{ 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1 },
+//				{ 1, 0, 1, 1, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 1 },
+//				{ 1, 0, 1, 1, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 1 },
+//				{ 1, 0, 0, 0, 0, 1, 1, 1, 1, 1, 0, 0, 1, 1, 1 },
+//				{ 1, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 1, 1, 0, 1 },
+//				{ 1, 0, 0, 1, 0, 0, 1, 1, 1, 0, 1, 1, 0, 0, 1 },
+//				{ 1, 0, 1, 1, 0, 1, 1, 0, 1, 0, 1, 0, 0, 0, 1 },
+//				{ 1, 0, 1, 0, 0, 0, 0, 0, 1, 0, 1, 1, 1, 0, 1 },
+//				{ 1, 0, 1, 0, 1, 1, 1, 1, 1, 0, 1, 0, 0, 0, 1 },
+//				{ 1, 0, 1, 0, 0, 0, 0, 0, 1, 1, 1, 0, 1, 0, 1 },
+//				{ 1, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 1 },
+//				{ 1, 0, 1, 0, 1, 1, 0, 1, 0, 1, 0, 1, 1, 0, 1 },
+//				{ 1, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+//				{ 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0 }};
+
+		MapStructure QuestionMap = new MapStructure(map, map.length, new Point(0, 0),
+				new Point(map.length-1, map.length-1));
+		
 		new Algorithm().Go(QuestionMap);
+//		QuestionMap.setMapStructure(new Point(map.length-1, map.length-1), new Point(0, 0));
+//		new Algorithm().Back(QuestionMap);	//从终点运行回去标记出所有的路径
+		
 		printAnswer(map);
+		
+		long endTime=System.currentTimeMillis();
+		System.out.println("This program takes " + (endTime - startTime) + "ms.");
 	}
+	
 	
 	public static void printAnswer(int[][] maps)
 	{
@@ -59,9 +85,10 @@ class Coord{
 
 class Point{
 	Coord coord;
-	Point parent;	
-	int Gx;		//代价函数
-	int Hx;	//估计函数
+	Point last;	
+	int Gx;		//移动距离
+	int Hx;	//离终点的距离
+	int air;
 	
 	//节点坐标
 	Point(int x, int y){
@@ -70,10 +97,13 @@ class Point{
 	
 	Point(Coord coord, Point parent, int Gx, int Hx){
 		this.coord = coord;
-		this.parent = parent;
+		this.last = parent;
 		this.Gx = Gx;
 		this.Hx = Hx;
-	}	
+	}
+	Point(int air){
+		this.air = air;
+	}
 }
 
 class MapStructure{
@@ -88,6 +118,11 @@ class MapStructure{
 		this.start = start;
 		this.end = end;
 	}
+	
+	void setMapStructure(Point start,Point end){
+		this.start = start;
+		this.end = end;
+	}
 }
 
 class Algorithm{
@@ -95,8 +130,8 @@ class Algorithm{
 	final static int sign = 2;
 	final static int value = 1; //只能上下左右移动
 	
-	ArrayList<Point> openList = new ArrayList<Point>();	//放入可移动节点
-	ArrayList<Point> closeList = new ArrayList<Point>();	//放入父节点和障碍节点
+	ArrayList<Point> openList = new ArrayList<Point>();		//放入可移动的路径
+	ArrayList<Point> closeList = new ArrayList<Point>();	//放入走过的路径
 	
 	boolean isEndCoord(Coord coord, Coord end){
 		if(coord != null && end.isEquals(coord))
@@ -111,7 +146,16 @@ class Algorithm{
 			return false;
 		if(isIncloseList(x, y))
 			return false;
+		if(isInopenList(x, y))
+			return false;
 		return true;
+	}
+	boolean isInopenList(int x, int y){
+		for(Point point : openList){
+			if(x == point.coord.x && y == point.coord.y)
+				return true;
+		}
+		return false;
 	}
 	
 	boolean isIncloseList(int x, int y){
@@ -138,7 +182,7 @@ class Algorithm{
 			
 			if(isEndCoord(goal.coord, end.coord)){
 				goal = end;
-				goal.parent = current;
+				goal.last = current;
 				goal.Gx = Gx;
 				goal.Hx = calcHx(goal.coord, end.coord);
 //				System.out.println("addPointInopenList➡canAddopenList➡isEndCoord.");
@@ -160,26 +204,94 @@ class Algorithm{
 		addPointInopenList(map, current, x, y-1);	//上
 		addPointInopenList(map, current, x, y+1);	//下
 	}
+
+	void drawPath(int[][] map, Point end){
+		while(end != null){
+			map[end.coord.y][end.coord.x] = sign;
+			end = end.last;
+		}
+	}
 	
-	void Path(int[][] map, Point end){
+	void getPathInformation(int[][] map, Point end){
 		int path = 1;
 		System.out.println("The shortest path needs " + end.Gx + " steps.");
 		while(end != null){
 			for(Point point : openList){
 				if(point.Hx == end.Hx && point.Gx == end.Gx){
 					path++;
-//					System.out.println("point.xy: " + point.coord.x + ", " + point.coord.y +  " end.xy: " 
-//							+ end.coord.x + ", " + end.coord.y);
-				}	
+				}
 			}
 			map[end.coord.y][end.coord.x] = sign;
-			end = end.parent;
+			end = end.last;
 		}
 		System.out.println("The shortest path number is " + path);
-		if(isIncloseList(9,8))
-			System.out.println("yes");
-		else
-			System.out.println("no");
+//		if(isIncloseList(9,8))
+//			System.out.println("yes");
+//		else
+//			System.out.println("no");
+	}
+	
+	//从地图左上角为所有的0赋予气
+	void addBreathbyLeft(MapStructure map){
+		int i = 0;
+		int j = 0;
+		Point zero = new Point(j, i);
+		for (i = 1; i < map.n-1; i++) {
+			for (j = 1; j < map.n-1; j++) {
+				zero.air = 0;
+				if(map.map[i][j] == 0){
+					if(map.map[i][j+1] == 0)
+						zero.air++;
+					if(j >= 1 && map.map[i][j-1] == 0)
+						zero.air++;
+					if(map.map[i+1][j] == 0)
+						zero.air++;
+					if(i >= 1 && map.map[i-1][j] == 0)
+						zero.air++;
+				}
+//				System.out.println("zero.air: " + zero.air);
+				if(zero.air <= 1){		//判断迷宫中只有一格气的点，说明它是死胡同，将它改为1
+					map.map[i][j] = bar;
+//					System.out.println("0➡1");
+				}
+			}
+		}
+	}
+	
+	//从地图右下角为所有的0赋予气
+		void addBreathbyRight(MapStructure map){
+			int i = 0;
+			int j = 0;
+			Point zero = new Point(j, i);
+			for (i = map.n-2; i > 0; i--) {
+				for (j = map.n-2; j > 0; j--) {
+					zero.air = 0;
+					if(map.map[i][j] == 0){
+						if(map.map[i][j+1] == 0)
+							zero.air++;
+						if(j >= 1 && map.map[i][j-1] == 0)
+							zero.air++;
+						if(map.map[i+1][j] == 0)
+							zero.air++;
+						if(i >= 1 && map.map[i-1][j] == 0)
+							zero.air++;
+					}
+//					System.out.println("zero.air: " + zero.air);
+					if(zero.air <= 1){		//判断迷宫中只有一格气的点，说明它是死胡同，将它改为1
+						map.map[i][j] = bar;
+//						System.out.println("0➡1");
+					}
+				}
+			}
+		}
+	
+	//从左上和右下两次遍历，将所有死胡同堵死
+	void changeBreath(MapStructure map){
+		for (int i = 0; i < map.map.length-1; i++) {
+			addBreathbyLeft(map);
+			addBreathbyRight(map);
+		}
+		System.out.println("Get rid of all dead paths (they don't have extra breath)");
 	}
 	
 	void Go(MapStructure map){
@@ -190,6 +302,18 @@ class Algorithm{
 		openList.clear();
 		closeList.clear();
 		
+		changeBreath(map);
+		
+		openList.add(map.start);
+		movePoint(map);
+		getPathInformation(map.map, map.end);
+		System.out.println("This is all maze paths (The sign of the pathway is '2'):");
+	}
+	
+	void Back(MapStructure map){
+		openList.clear();
+		closeList.clear();
+		
 		openList.add(map.start);
 		movePoint(map);
 	}
@@ -197,26 +321,22 @@ class Algorithm{
 	Point findMinInopenList(){
 		Point goal = openList.get(0);
 		for(Point point : openList){
-			if(point.Gx + point.Hx < goal.Gx + point.Hx)
+			if(point.Gx + point.Hx < goal.Gx + goal.Hx)
 				goal = point;
 		}
 		return goal;
 	}
 	
 	void movePoint(MapStructure map){
-//		int count = 0;
 		while(!openList.isEmpty()){
 			Point goal = findMinInopenList();
 			openList.remove(goal);
 			closeList.add(goal);
 			addPointInopenList(map, goal);
-//			count++;
 			if(isIncloseList(map.end.coord.x, map.end.coord.y)){
-				Path(map.map, map.end);
-				System.out.println("This is the shortest path.");
+				drawPath(map.map, map.end);
 				break;
 			}
-//			System.out.println("movePoint runs:" + count);
 		}
 	}
 }
